260a845fba5666b0f966c4e59c2bc464c07a236e
marvinkhywah <khywah710@gmail.com>
2019-07-26 15:02:30 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Windows Sysinfo
+
+A PowerShell script that runs a few basic system checks. It's using a menu interface.
+
+# Requirements
+
+Admin access is needed to run the script as well as Remote Server Administation Tool (RSAT) installed
---
+++ src/system_check.ps1
@@ -0,0 +1,75 @@
+﻿
+function Show-Menu
+{
+    param(
+        [string]$Title = 'My Menu'
+    )
+    cls
+    Write-Host "================ $Testing ================"
+
+     Write-Host "1: Is the Domain reachable?"
+     Write-Host "2: OS info."
+     Write-Host "3: Disk Space Check"
+     Write-Host "4: Network Info"
+     Write-Host "5: Check software installs"
+     Write-Host "6: Are the desktop shortcuts okay?"
+     Write-Host "7: Are the desktop shortcuts okay?"
+     Write-Host "Q: Press 'Q' to quit."
+}
+
+function Guts
+{
+do
+{ 
+     Show-Menu
+     $input = Read-Host "Please make a selection"
+     switch ($input)
+     {
+           '1' {
+                cls
+                'You chose option #1'
+                Test-ComputerSecureChannel -Server "your donmain here" ##you can put your company's domain info here
+                    if ('True'){
+                        'You can reach our domain!'
+                         } Else{
+                         'Cannot reach it :('
+                         }
+           } '2' {
+                cls
+                'You chose option #2'
+                (Get-WMIObject win32_operatingsystem).name
+                (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+                (Get-WmiObject Win32_OperatingSystem).CSName
+      
+           } '3' {
+                cls
+                'You chose option #3'
+                Get-PSDrive
+           }  '4' {
+                cls
+                'You chose option #4'
+                Get-NetIPAddress –AddressFamily IPv4 | Select-Object IPAddres                 
+           }  
+              '5'{
+                Get-Service -DisplayName "Windows Defnder Advanced Thread Protection Service"
+
+           }  
+              '6'{
+                Get-Childitem C:\Users\Public\Desktop
+
+           } 
+              'q'{
+                return
+           }
+                default{
+                cls
+                'Please enter 1-5'
+           }
+     }
+     pause
+}
+until ($input -eq 'q')
+}
+
+fun
+Guts
